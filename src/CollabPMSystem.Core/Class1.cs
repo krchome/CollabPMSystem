54eb81c2df1596ef38bc1bd235aa6e8b5e2cbab6
Kaushik Roy Chowdhury <devedkaushik@gmail.com>
2025-08-18 21:59:05 +1200

    Staged changes

--- src/CollabPMSystem.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CollabPMSystem.Core;
-
-public class Class1
-{
-
-}
---
+++ src/CollabPMSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,99 @@
+﻿using CollabPMSystem.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabPMSystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Define DbSet properties for your entities here
+        // Example:
+
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Attachment> Attachments => Set<Attachment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional model configuration can be done here
+            //Project
+            modelBuilder.Entity<Project>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Name).IsRequired().HasMaxLength(200);
+                entity.Property(p => p.Description).HasMaxLength(1000);
+
+                entity.HasMany(p => p.Tasks)
+                    .WithOne()
+                    .HasForeignKey(t => t.ProjectId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                entity.HasMany(p => p.Teams)
+                    .WithOne()
+                    .HasForeignKey(t => t.ProjectId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            });
+            //TaskItem
+            modelBuilder.Entity<TaskItem>(entity =>
+            {
+                entity.HasKey(t => t.Id);
+                entity.Property(t => t.Title).IsRequired().HasMaxLength(200);
+                entity.Property(t => t.Status).IsRequired().HasMaxLength(50);
+                entity.HasMany(t => t.Comments)
+                    .WithOne()
+                    .HasForeignKey(c => c.TaskItemId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+            //User
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Username).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.Email).IsRequired().HasMaxLength(250);
+
+            });
+            //Team
+            modelBuilder.Entity<Team>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Name).IsRequired().HasMaxLength(200);
+                entity.Property(u => u.Description).HasMaxLength(1000);
+                entity.HasMany(t => t.Members)
+                    .WithMany(u => u.Teams);
+
+            });
+            //Comment
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Content).IsRequired().HasMaxLength(2000);
+
+            });
+            //Attachment
+            modelBuilder.Entity<Attachment>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+                entity.Property(a => a.FileName).IsRequired().HasMaxLength(255);
+                entity.Property(a => a.FilePath).IsRequired().HasMaxLength(500);
+                
+            });
+
+
+        }
+
+
+    }
+}
+
