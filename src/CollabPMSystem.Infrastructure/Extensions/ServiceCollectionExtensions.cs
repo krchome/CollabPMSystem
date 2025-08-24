a62c3547ee7aac79daf81c95d6a6c85410808678
Kaushik Roy Chowdhury <devedkaushik@gmail.com>
2025-08-24 18:05:36 +1200

    v2 Incorporating domain entities in the Core project.

---
+++ src/CollabPMSystem.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CollabPMSystem.Infrastructure.Data;
+
+
+namespace CollabPMSystem.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            {     
+            services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                });
+                // Add other infrastructure services as needed
+                return services;
+            }
+        }
+    }
+}
